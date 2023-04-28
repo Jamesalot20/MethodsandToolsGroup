779257185e0206a3bec110920791be0607e2aef2
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-27 21:28:27 -0500

    Update productsController.js

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -22,3 +22,39 @@ exports.getProducts = async (req, res) => {
     res.status(500).json({ error: 'An error occurred while fetching products.' });
   }
 };
+
+// Function to handle command line interface for getting products
+async function getProductsCLI(rl) {
+  rl.question('Enter a search keyword (leave empty for no keyword): ', async (search) => {
+    rl.question('Enter a category (leave empty for no category): ', async (category) => {
+      try {
+        let query = {};
+
+        if (search) {
+          query.name = { $regex: search, $options: 'i' };
+        }
+
+        if (category) {
+          query.category = category;
+        }
+
+        const products = await Product.find(query);
+
+        if (products.length === 0) {
+          console.log('No products found.');
+        } else {
+          console.log('Products:');
+          products.forEach((product) => {
+            console.log(`- ${product.name} (Category: ${product.category})`);
+          });
+        }
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+      // Call your mainMenu function here to return to the main menu
+      mainMenu();
+    });
+  });
+}
+
+exports.getProductsCLI = getProductsCLI;
