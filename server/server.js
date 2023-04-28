123b7345704c9c69352f1a812c96eaaf0aefa228
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-27 20:43:51 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const readline = require('readline');
 const dotenv = require('dotenv');
 const connectToDB = require('./db');
+const { registerUser, loginUser, logoutUser, deleteUser } = require('./controllers/usersController');
 
 // Load environment variables
 dotenv.config();
@@ -11,32 +12,42 @@ const rl = readline.createInterface({
 });
 
 function mainMenu() {
-    console.log('\nWelcome to TechTonic');
-    console.log('Please choose an option:');
-    console.log('1. Register');
-    console.log('2. Login');
-    console.log('3. Quit');
-  
-    rl.question('\nEnter your choice: ', (choice) => {
-      switch (choice) {
-        case '1':
-          registerUser();
-          break;
-        case '2':
-          loginUser();
-          break;
-        case '3':
-          quit();
-          break;
-        default:
-          console.log('Invalid choice. Please try again.\n');
-          mainMenu();
-      }
-    });
-  
+  console.log('\nWelcome to TechTonic');
+  console.log('Please choose an option:');
+  console.log('1. Register');
+  console.log('2. Login');
+  console.log('3. Logout');
+  console.log('4. Delete account');
+  console.log('5. Quit');
+
+  rl.question('\nEnter your choice: ', (choice) => {
+    switch (choice) {
+      case '1':
+        registerUser(rl);
+        break;
+      case '2':
+        loginUser(rl);
+        break;
+      case '3':
+        logoutUser();
+        break;
+      case '4':
+        deleteUser();
+        break;
+      case '5':
+        quit();
+        break;
+      default:
+        console.log('Invalid choice. Please try again.\n');
+        mainMenu();
+    }
+  });
 }
 
-// ... Implement other functions like registerUser, loginUser, etc.
+function quit() {
+  console.log('Goodbye!');
+  rl.close();
+}
 
 connectToDB()
   .then(() => {
