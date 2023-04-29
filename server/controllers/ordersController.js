062fbf52057cef76578223d4bfd32652804503d2
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-28 23:03:25 -0500

    Update ordersController.js

--- server/controllers/ordersController.js
+++ server/controllers/ordersController.js
@@ -11,21 +11,6 @@ exports.getOrdersByUser = async (req, res) => {
   }
 };
 
-
-
-exports.getOrderById = async (req, res) => {
-  try {
-    const order = await Order.findById(req.params.id);
-    if (!order) {
-      res.status(404).json({ error: 'Order not found.' });
-    } else {
-      res.status(200).json(order);
-    }
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred while fetching the order.' });
-  }
-};
-
 exports.createOrder = async (req, res) => {
   try {
     const order = new Order(req.body);
@@ -36,18 +21,7 @@ exports.createOrder = async (req, res) => {
     res.status(500).json({ error: 'An error occurred while creating the order.' });
   }
 };
-exports.updateOrder = async (req, res) => {
-  try {
-    const order = await Order.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!order) {
-      res.status(404).json({ error: 'Order not found.' });
-    } else {
-      res.status(200).json(order);
-    }
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred while updating the order.' });
-  }
-};
+
 exports.deleteOrder = async (req, res) => {
   try {
     const order = await Order.findByIdAndRemove(req.params.id);
@@ -58,15 +32,5 @@ exports.deleteOrder = async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({ error: 'An error occurred while deleting the order.' });
-  }
-};
-
-exports.getBuyerOrderHistory = async (req, res) => {
-  try {
-    const orders = await Order.find({ userId: req.user.userId });
-    res.status(200).json(orders);
-  } catch (error) {
-    console.error('Error fetching order history:', error);
-    res.status(500).json({ error: 'An error occurred while fetching the order history.' });
   }
 };
